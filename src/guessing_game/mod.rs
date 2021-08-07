46e4c958175e54c6e2ab15f467f591d50feade2e
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-07 01:16:41 -0300

    feat: processing a guess

---
+++ src/guessing_game/mod.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+/// Stopped [here](https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html#generating-a-secret-number)
+pub fn execute() {
+    println!("Guess the number!");
+
+    println!("Please, input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
