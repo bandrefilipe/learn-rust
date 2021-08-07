a186c6d445e7c6b04146255ab6d53464c972e2d6
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-07 01:16:41 -0300

    feat: processing a guess

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+mod guessing_game;
 mod hello_world;
 
 fn main() {
     topic_1_2();
+    topic_2();
 }
 
 fn topic_1_2() {
     hello_world::execute();
 }
+
+#[allow(dead_code)]
+fn topic_2() { guessing_game::execute(); }
