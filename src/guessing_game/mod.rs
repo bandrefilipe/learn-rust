819e1b6957425e485236482867f8c70432e71bcd
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-07 22:37:47 -0300

    feat: implement the guessing game

--- src/guessing_game/mod.rs
+++ src/guessing_game/mod.rs
@@ -1,16 +1,40 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
-/// Stopped [here](https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html#generating-a-secret-number)
+/// Generates a random number between 1-100 (inclusive) and prompts the user for guessing the answer.
+/// While the guessed number is incorrect, hints at the user if the guess is too small or big.
+///
+/// The game finishes when the user guesses correctly.
 pub fn execute() {
     println!("Guess the number!");
 
-    println!("Please, input your guess.");
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please, input your guess: ");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
