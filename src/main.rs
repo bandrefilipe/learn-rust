43c477ad6136494a2c28f910db77a1393be3b0b9
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-09 23:11:19 -0300

    feat: topic functions

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod guessing_game;
 mod hello_world;
 mod variables;
 mod data_types;
+mod functions;
 
 fn main() {
     // hello_world::execute();
     // guessing_game::execute();
     // variables::execute();
-    data_types::execute();
+    // data_types::execute();
+    functions::execute();
 }
