80c0b9a4e7c45133f2cffa002538294326c888b2
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-09 23:11:19 -0300

    feat: topic functions

---
+++ src/functions/mod.rs
@@ -0,0 +1,27 @@
+#[allow(dead_code)]
+pub fn execute() {
+    println!("Some function");
+    another_function(5, 6);
+
+    // statements & expressions:
+    let _x = 5;
+    let _y = {
+        let x = 3;
+        x + 1 // no ;
+    }; // _y == 4
+    let _five = five();
+    let _six = plus_one(five());
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(n: i32) -> i32 {
+    n + 1
+}
