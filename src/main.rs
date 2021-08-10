97fdf6d4ab433d5c5587af9b8541cda85d20121f
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-09 23:40:32 -0300

    feat: topic control flow with if statements

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod hello_world;
 mod variables;
 mod data_types;
 mod functions;
+mod branches;
 
 fn main() {
     // hello_world::execute();
     // guessing_game::execute();
     // variables::execute();
     // data_types::execute();
-    functions::execute();
+    // functions::execute();
+    branches::execute();
 }
