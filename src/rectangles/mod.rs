840115280026aa1ae2d8ab876e9053399dbde4c3
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-16 23:56:33 -0300

    feat: topic method syntax

--- src/rectangles/mod.rs
+++ src/rectangles/mod.rs
@@ -1,5 +1,11 @@
 #[allow(dead_code)]
 pub fn execute() {
+    defining_methods();
+    methods_with_more_parameters();
+    associated_functions();
+}
+
+fn defining_methods() {
     let rect = Rectangle {
         width: 30,
         height: 50,
@@ -8,8 +14,31 @@ pub fn execute() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect)
+        rect.area()
     );
+}
+
+fn methods_with_more_parameters() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+
+fn associated_functions() {
+    let sq = Rectangle::square(3);
+    println!("Debug: sq={:?}", sq);
 }
 
 #[derive(Debug)]
@@ -18,6 +47,19 @@ struct Rectangle {
     height: u32,
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
