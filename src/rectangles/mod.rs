bbba97ebb4fe43716e10488cc8bb6e6eca526b61
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-16 23:31:25 -0300

    feat: topic an example program using structs

---
+++ src/rectangles/mod.rs
@@ -0,0 +1,23 @@
+#[allow(dead_code)]
+pub fn execute() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("Debug: {:#?}", rect);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect)
+    );
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
