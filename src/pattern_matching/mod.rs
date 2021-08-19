3c9e5e3c6b2b77f72b4edf2de51f93ca2d6cf60e
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-18 22:02:57 -0300

    feat: topic patterns that bind to values

---
+++ src/pattern_matching/mod.rs
@@ -0,0 +1,40 @@
+#[allow(dead_code)]
+pub fn execute() {
+    patterns_that_bind_to_values();
+}
+
+fn patterns_that_bind_to_values() {
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Nickel);
+    value_in_cents(Coin::Dime);
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
