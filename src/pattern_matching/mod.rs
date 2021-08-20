097992fba0c39171611970585bfc0aa0e94f61fc
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-19 22:59:22 -0300

    feat: topic concise control flow with if let

--- src/pattern_matching/mod.rs
+++ src/pattern_matching/mod.rs
@@ -1,7 +1,10 @@
+use crate::pattern_matching::UsState::Alabama;
+
 #[allow(dead_code)]
 pub fn execute() {
     patterns_that_bind_to_values();
     matching_with_option();
+    if_let();
 }
 
 fn patterns_that_bind_to_values() {
@@ -20,6 +23,38 @@ fn matching_with_option() {
     some_u8_value(1);
     some_u8_value(7);
     some_u8_value(255);
+}
+
+fn if_let() {
+    // this:
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+    // can be written as this:
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    // also, this...
+    let mut _count = 0;
+    let coin = some_coin();
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}", state),
+        _ => _count += 1,
+    }
+    // can be writeen as this:
+    let coin = some_coin();
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}", state);
+    } else {
+        _count += 1;
+    }
+}
+
+fn some_coin() -> Coin {
+    Coin::Quarter(Alabama)
 }
 
 #[allow(dead_code)]
