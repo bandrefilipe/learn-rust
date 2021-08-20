7ea26bb5fa6ab9e3da821d5b562ca5d982dab23b
bandrefilipe <b.andrefilipe@gmail.com>
2021-08-20 03:38:58 -0300

    feat: topic paths for referring to an item in the module tree

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,71 @@
+mod front_of_house {
+
+    pub mod hosting {
+
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path:
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path:
+    front_of_house::hosting::add_to_waitlist();
+
+    // order a breakfast in the summer with rye toast:
+    let mut meal = back_of_house::Breakfast::summer("rye");
+    // change our mind about what bread we'd like:
+    meal.toast = String::from("wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // the next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+
+fn serve_order() {}
+
+mod back_of_house {
+    
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();        
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
